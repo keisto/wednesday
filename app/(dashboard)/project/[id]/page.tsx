e629e9728358e187b03e0d495e47f621e00dbc5d
Tony Keiser <keisertony@gmail.com>
2022-12-27 08:38:19 -0800

    Added project view page

--- app/(dashboard)/project/[id]/page.tsx
+++ app/(dashboard)/project/[id]/page.tsx
@@ -1,3 +1,30 @@
-export default function Project() {
-  return <h1>Project</h1>
+import { cookies } from 'next/headers'
+
+import { db } from '@/lib/db'
+import TasksCard from '@/components/TaskCard'
+import { getUserFromCookie } from '@/lib/auth'
+
+const getData = async (id: string) => {
+  const user = await getUserFromCookie(cookies())
+  const project = await db.project.findFirst({
+    where: {
+      id,
+      ownerId: user?.id,
+    },
+    include: {
+      tasks: true,
+    },
+  })
+
+  return project
+}
+
+export default async function Project({ params }: { params: { id: string } }) {
+  const project = await getData(params.id)
+  return (
+    <div className="h-full overflow-y-auto pl-6 w-full">
+      {/* @ts-expect-error Server Component */}
+      <TasksCard tasks={project.tasks} title={project.name} />
+    </div>
+  )
 }
